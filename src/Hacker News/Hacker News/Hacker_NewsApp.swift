f9c8af74e2757d4cf8fe5dd6f563557b5e5a6563
Bohirjon Akhmedov <bohir@silkroadprofessionals.com>
2021-01-17 21:26:20 +0500

    init commit

---
+++ src/Hacker News/Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Bohirjon Akhmedov on 17/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
